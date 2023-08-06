96ae6f42e0787555d13aef0b04748c0b1152c095
Pam <pwillisop@yahoo.com>
2023-08-06 12:23:37 -0400

    attempting what if all are no

--- Develop/script.js
+++ Develop/script.js
@@ -2,58 +2,56 @@
 var lowercaseCharacters =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseCharacters =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var num =["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var numbers =["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // var i=["How many characters would you like between 8 and 12?","Do you want Upper Case letters?","Do you want Lower Case letters?","Do you want Numerals?","Do you want Special Characters?"]
+
+var tempUpCase = [];
+var tempLowCase = [];
+var tempNumChar = [];
+var tempSpecChar = [];
+var passwordLength;
 var generateBtn = document.querySelector("#generate");
 function generatePassword () {
-  var response=prompt("How many characters would you like between 8 and 12?") 
-
+var response=prompt("How many characters would you like between 8 and 12?") 
   if (response < 8) {
-    response=prompt("Not a valid response"); }
+    response=alert("Not a valid response"); }
       else if (response > 12 && response <=128) {
-        response=prompt("Not a valid response"); }
+        response=alert("Not a valid response"); }
         else {
-          response=NumofChar;
-      }
-}
-function generatePassword () {
-  var response=prompt("Do you want to use uppercase characters, y or n");
-
-  if (response = "n") {
-    response=null;
+          passwordLength=response;
+        }
+var responseUpCase=prompt("Do you want Upper Case letters?") 
+  if (responseUpCase = "y") {
+    tempUpCase.push(uppercaseCharacters);
   } else {
-    response=useUpperCaseChar;
+    responseUpCase=false;
   }
-}
-function generatePassword () {
-  var response=prompt("Do you want to use lowercase characters, y or n");
-
-  if (response = "n") {
-    response=null;
-  } else {
-    response=useLowerCaseChar;
+var responseLowCase=prompt("Do you want Lower Case letters?")
+  if (responseLowCase = "y") {
+    tempLowCase.push(lowercaseCharacters);
+    } else {
+    responseLowCase=false;
   }
-}
-function generatePassword () {
-  var response=prompt("Do you want to use numerical characters, y or n");
-
-  if (response = "n") {
-    response=null;
-  } else {
-    response=useNumberChar;
-  }
-}
-function generatePassword () {
-  var response=prompt("Do you want to use special characters, y or n");
-
-  if (response = "n") {
-    response=null;
-  } else {
-    response=useSpecialChar;
-  }
+  var responseNumChar=prompt("Do you want Numerals?")
+  if (responseNumChar = "y") {
+      tempNumChar.push(numbers);
+    } else {
+      responseNumChar=false;
+    }
+    var responseSpecChar=prompt("Do you want Special Characters?")
+  if (responseSpecChar = "y") {
+      tempSpecChar.push(specialCharacters);
+    } else {
+      responseSpecChar=false;}
+  var totalResponse=(responsetempUpCase + responsetempLowCase + responsetempNumChar + responsetempSpecChar) 
+  if (totalResponse === false) {
+      alert("not enought information. A password can not be generated");
+    }
 }
 
+arr=[tempUpCase, tempLowCase, tempNumChar, tempSpecChar];
+console.log (arr);
 
 
 
