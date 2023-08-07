f0a40c9bdfc8f732318dd7db64a60d37067fc50b
Pam <pwillisop@yahoo.com>
2023-08-07 17:24:29 -0400

    for statement

--- Develop/script.js
+++ Develop/script.js
@@ -17,66 +17,67 @@ var response=prompt("How many characters would you like between 8 and 128?")
         else {
           passwordLength.push(response);
         }
-var responseUpCase=prompt("Do you want Upper Case letters?") 
+var responseUpCase=prompt("Do you want Upper Case letters? y/n") 
   if (responseUpCase === "y") {
     possibleCharacters.push(uppercaseCharacters);
     } else {
       noResponseCount++;
   }
-var responseLowCase=prompt("Do you want Lower Case letters?")
+var responseLowCase=prompt("Do you want Lower Case letters? y/n")
   if (responseLowCase === "y") {
     possibleCharacters.push(lowercaseCharacters);
     } else {
       noResponseCount++;
   }
-var responseNumChar=prompt("Do you want Numerals?")
+var responseNumChar=prompt("Do you want Numerals? y/n")
   if (responseNumChar === "y") {
     possibleCharacters.push(numbers);
     } else {
       noResponseCount++;
   }
-var responseSpecChar=prompt("Do you want Special Characters?")
+var responseSpecChar=prompt("Do you want Special Characters? y/n")
   if (responseSpecChar === "y") {
     possibleCharacters.push(specialCharacters);
     } else {
       noResponseCount++;
   }
   if (noResponseCount===4) {
-      alert("not enough information. A password can not be generated"); 
+      alert("not enough information. A password can not be generated y/n"); 
   } else  {
       console.log("proceed");
-  }
+  }}
 
 possibleCharacters = possibleCharacters.flat();
 console.log(possibleCharacters);
 console.log(passwordLength);
-}
-
-
-var password =[];
-
-// number of values
-let total = possibleCharacters.length;
-
-for (i=0; i < passwordLength; i++)
-{ 
-  return Math.floor(Math.random() * total);
-  possibleCharacters.push(password); }
-
-console.log(password);
-
-
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ 
+var passwordText [];
+var randomCharacter=[];
 
-}
 
-// Add event listener to generate button
+    for (var i=0; i < passwordLength; i++) { 
+      var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+      console.log(randomIndex);
+      randomCharacter = possibleCharacters[randomIndex];
+      console.log(randomCharacter);
+    } 
+      while (i<=passwordLength) {
+        randomCharacter.push(passwordText);
+        password=passwordText.flat();
+      console.log(passwordText); 
+    }
+  
+    passwordText = writePassword();
+
+    // var passwordText = document.querySelector("#password");
+  
+    // passwordText.value = password;
+
+
+// // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+  
