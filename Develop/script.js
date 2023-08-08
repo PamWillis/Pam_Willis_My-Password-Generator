2867adee639b1ee6d93e703643ea84388c1a035d
Pam <pwillisop@yahoo.com>
2023-08-08 17:00:45 -0400

    question multiple if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,83 +1,129 @@
 // Assignment Code
-var lowercaseCharacters =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseCharacters =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var numbers =["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-var possibleCharacters =[];
-var passwordLength =[];
+var possibleCharacters = [];
+// var passwordLength =[];
 var noResponseCount = 0;
 
 var generateBtn = document.querySelector("#generate");
-function generatePassword () {
-var response=prompt("How many characters would you like between 8 and 128?") 
-  if (response < 8 || response > 128) {
-    response=alert("Not a valid response");
-    return null; }
-        else {
-          passwordLength.push(response);
-        }
-var responseUpCase=prompt("Do you want Upper Case letters? y/n") 
-  if (responseUpCase === "y") {
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like between 8 and 128?")
+  console.log(isNaN(passwordLength));
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Not a valid response");
+    return null;
+  }
+
+  var responseUpCase = confirm("Do you want Upper Case letters?")
+  console.log(responseUpCase);
+  if (responseUpCase) {
     possibleCharacters.push(uppercaseCharacters);
-    } else {
-      noResponseCount++;
+  } else {
+    noResponseCount++;
   }
-var responseLowCase=prompt("Do you want Lower Case letters? y/n")
-  if (responseLowCase === "y") {
+  var responseLowCase = confirm("Do you want Lower Case letters?")
+  if (responseLowCase) {
     possibleCharacters.push(lowercaseCharacters);
-    } else {
-      noResponseCount++;
+  } else {
+    noResponseCount++;
   }
-var responseNumChar=prompt("Do you want Numerals? y/n")
-  if (responseNumChar === "y") {
+  var responseNumChar = confirm("Do you want Numerals?")
+  if (responseNumChar) {
     possibleCharacters.push(numbers);
-    } else {
-      noResponseCount++;
+  } else {
+    noResponseCount++;
   }
-var responseSpecChar=prompt("Do you want Special Characters? y/n")
-  if (responseSpecChar === "y") {
+  var responseSpecChar = confirm("Do you want Special Characters?")
+  if (responseSpecChar) {
     possibleCharacters.push(specialCharacters);
-    } else {
-      noResponseCount++;
+  } else {
+    noResponseCount++;
   }
-  if (noResponseCount===4) {
-      alert("not enough information. A password can not be generated y/n"); 
-  } else  {
-      console.log("proceed");
-  }}
+  if (noResponseCount === 4) {
+    alert("not enough information. A password can not be generated");
+    return
+  }
 
-possibleCharacters = possibleCharacters.flat();
-console.log(possibleCharacters);
-console.log(passwordLength);
+  console.log(noResponseCount);
+  console.log(possibleCharacters);
+  console.log(passwordLength);
+  possibleCharacters = possibleCharacters.flat();
+  console.log(possibleCharacters);
 
+  //if responseLowCase is true then it must pe in passwordText
+  //if responseUpCase is true then it must pe in passwordText 
+  //if responseNumChar is true then it must pe in passwordText 
+  //if responseSpecChar is true then it must pe in passwordText 
+  //count how many true described by noResponseCount
+  //subtract that number from passwordLength
+  //remainder can use randomIndex
+
+  //create a for loop for each response that is true, push random value into password
+  //subtract noResponseCount from passwordLength, run the rest
+  //
+
+  // Write password to the #password input
+
+  var passwordText = [];
+
+  for (var i = 0; i < passwordLength; i++) {
+    //first four ifs are to make sure one of each is in password
+    //question if all if statements need to be combined some how
+    if (responseUpCase === true) {
+      var randomIndex = Math.floor(Math.random() * uppercaseCharacters.length);
+      var value = uppercaseCharacters[randomIndex];
+      console.log(value);
+      passwordText.push(value);
+    }
+    if (responseLowCase === true) {
+      var randomIndex = Math.floor(Math.random() * lowercaseCharacters.length);
+      var value = lowercaseCharacters[randomIndex];
+      console.log(value);
+      passwordText.push(value);
+    }
+    if (responseNumChar === true) {
+      var randomIndex = Math.floor(Math.random() * numbers.length);
+      var value = numbers[randomIndex];
+      console.log(value);
+      passwordText.push(value);
+    }
+    if (responseSpecChar === true) {
+      var randomIndex = Math.floor(Math.random() * specialCharacters.length);
+      var value = specialCharacters[randomIndex];
+      console.log(value);
+      passwordText.push(value);
+    }
+    //This is to say # that are true minus passwordLength-not working
+    passwordLength = (passwordLength -= noResponseCount);
+    console.log(passwordLength);
+    //Last one is to put remainder in (not looping)
+    if (passwordLength > passwordText.length) {
+      var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+      var value = possibleCharacters[randomIndex];
+      console.log(value);
+      passwordText.push(value);
+    }
+    //Joining selections together
+    console.log(passwordText);
+    var Joinpasswordtext = passwordText.join(" ")
+    console.log(Joinpasswordtext);
+    return Joinpasswordtext;
+
+  }
+}
 
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
- 
-var passwordText [];
-var randomCharacter=[];
+  passwordText.value = password;
 
+}
 
-    for (var i=0; i < passwordLength; i++) { 
-      var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
-      console.log(randomIndex);
-      randomCharacter = possibleCharacters[randomIndex];
-      console.log(randomCharacter);
-    } 
-      while (i<=passwordLength) {
-        randomCharacter.push(passwordText);
-        password=passwordText.flat();
-      console.log(passwordText); 
-    }
-  
-    passwordText = writePassword();
-
-    // var passwordText = document.querySelector("#password");
-  
-    // passwordText.value = password;
-
-
-// // Add event listener to generate button
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  
+
